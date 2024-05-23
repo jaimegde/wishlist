9119099d9f1ca136b17e3c7a3820a7caa002b42d
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-23 19:10:09 +0200

    added floating button

--- app/src/main/java/com/example/wishlist/HomeView.kt
+++ app/src/main/java/com/example/wishlist/HomeView.kt
@@ -5,9 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(){
@@ -15,10 +21,23 @@ fun HomeView(){
     Scaffold(
         topBar = {AppBarView(title = "WishList", {
             Toast.makeText(context,"Button Clicked", Toast.LENGTH_LONG).show()
+        })},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {
 
-        })}
+                })
+             {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
     ){
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
 
         }
     }

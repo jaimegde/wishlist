9ece3f53803c65a3942d4c1a10f5f4ca9eaed42a
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-23 17:35:46 +0200

    added back button

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -18,7 +18,7 @@ import com.example.wishlist.ui.theme.WishlistTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             WishlistTheme {
                Surface(

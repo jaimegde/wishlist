8fce867cddee5c7a12918768d5890df5f33a9da3
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-24 17:21:00 +0200

    back butotn works

--- app/src/main/java/com/example/wishlist/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlist/AddEditDetailView.kt
@@ -39,6 +39,7 @@ fun AddEditDetailView(
         topBar = { AppBarView(title=
         if(id != 0L) stringResource(id = R.string.update_wish)
         else stringResource(id = R.string.add_wish))
+        {navController.navigateUp()}
         }
     ) {
         Column(modifier = Modifier

b216fbd0e02d863bc868fe71c640468f9e77339f
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-28 13:18:21 +0200

    properly deletes

--- app/src/main/java/com/example/wishlist/HomeView.kt
+++ app/src/main/java/com/example/wishlist/HomeView.kt
@@ -9,12 +9,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -26,6 +33,7 @@ import androidx.navigation.NavController
 import com.example.wishlist.data.DummyWish
 import com.example.wishlist.data.Wish
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun HomeView(
     navController: NavController,
@@ -55,11 +63,30 @@ fun HomeView(
         LazyColumn(modifier = Modifier
             .fillMaxSize()
             .padding(it)) {
-            items(wishlist.value){
-                wish -> WishItem(wish = wish) {
-                    val id = wish.id
-                navController.navigate(Screen.AddScreen.route + "/$id")
-            }
+            items(wishlist.value, key={wish -> wish.id}){
+                wish ->
+                val dismissState = androidx.compose.material.rememberDismissState(
+                    confirmStateChange = {
+                        if(it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart){
+                            viewModel.deleteWish(wish)
+                        }
+                        true
+                    }
+                )
+
+
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {},
+                    directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+                    dismissThresholds = {FractionalThreshold(0.25f)},
+                    dismissContent = {
+                        WishItem(wish = wish) {
+                            val id = wish.id
+                            navController.navigate(Screen.AddScreen.route + "/$id")
+                        }
+                    })
+
             }
         }
     }
